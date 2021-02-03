a45771a402fd38ea905091137f8617447267bf00
Lawliet L <zexceed12300@gmail.com>
2021-02-03 22:30:22 +0700

    usb: gadget: u_ether: fix incomplete type of rndis_packet_msg_type
    in newer linux kernel, size of rndis_packet_msg_type is actually 44
    
    Signed-off-by: Lawliet L <zexceed12300@gmail.com>

--- drivers/usb/gadget/function/u_ether.c
+++ drivers/usb/gadget/function/u_ether.c
@@ -2074,8 +2074,8 @@ struct net_device *gether_connect(struct gether *link)
 	 * req->buf which is allocated later
 	 */
 	if (!dev->sg_enabled) {
-		link->header = kzalloc(sizeof(struct rndis_packet_msg_type),
-						GFP_ATOMIC);
+		/* size of rndis_packet_msg_type is 44 */
+		link->header = kzalloc(44, GFP_ATOMIC);
 		if (!link->header) {
 			pr_err("RNDIS header memory allocation failed.\n");
 			result = -ENOMEM;
